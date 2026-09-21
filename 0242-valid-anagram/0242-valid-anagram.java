7104892f5b13da04824c1e99c8e653eb79232363
RaviChauhan <1chauhanravi0@gmail.com>
2026-09-21 11:37:09 +0530

    Time: 10 ms (29.07%), Space: 43.4 MB (99.95%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        int s_p=1, t_p=1,s_s=0,t_s=0,s_0=0,t_0=0;
+        for(int i =0;i<s.length();i++){
+            s_p*=(int)s.charAt(i);
+            t_p*=(int)t.charAt(i);
+            s_s+=(int)s.charAt(i);
+            t_s+=(int)t.charAt(i);
+            s_0^=(int)s.charAt(i);
+            t_0^=(int)t.charAt(i);
+
+        }
+        if(s_p==t_p && s_s==t_s && s_0!=0 && t_0!=0 && s_0==t_0) return true;
+        return false;
+    }
+}
